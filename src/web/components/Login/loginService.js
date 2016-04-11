79448260e8bbd4956ba1db52e4108f88e047db7e
igor.onyshchenko <igor.onyshchenko@gmail.com>
2016-04-11 22:33:42 +0300

    md5 password

--- src/web/components/Login/loginService.js
+++ src/web/components/Login/loginService.js
@@ -1,6 +1,11 @@
+import md5 from 'md5';
+
+
 export default {
     login(email, pass) {
-        return fetch(`/api/login/${email}/${pass}`)
+        let passMd5 = md5(pass);
+        
+        return fetch(`/api/login/${email}/${passMd5}`)
             .then(data => data.text())
             .then(resData => JSON.parse(resData));
     }
